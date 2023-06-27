bb128ceb3086404c8af2e290ce244ca7d0b8538b
Casey Carroll <caseydean8@gmail.com>
2023-06-27 19:09:22 -0400

    Added next.js view

--- next-client/next.config.js
+++ next-client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // set to false for semantic-ui button
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
