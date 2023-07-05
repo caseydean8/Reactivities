ca3d6993fd1aeaffbec584483a0068d2928ad1f0
Casey Carroll <caseydean8@gmail.com>
2023-07-05 17:05:56 -0400

    Added home, activities, createactivities routes

--- client-app/src/router/Routes.tsx
+++ client-app/src/router/Routes.tsx
@@ -1,10 +1,18 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../app/layout/App";
+import HomePage from "../features/home/HomePage";
+import ActivityForm from "../features/activities/form/ActivityForm";
+import ActivityDashboard from "../features/activities/dashboard/ActivityDashboard";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "activities", element: <ActivityDashboard /> },
+      { path: "createActivity", element: <ActivityForm /> },
+    ],
   },
 ];
 
