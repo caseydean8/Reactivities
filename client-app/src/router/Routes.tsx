14d019d7c8c7ad3e662fb1296713ac5c9c70bc59
Casey Carroll <caseydean8@gmail.com>
2023-07-06 16:33:41 -0400

    Remove nav from home page

--- client-app/src/router/Routes.tsx
+++ client-app/src/router/Routes.tsx
@@ -1,6 +1,5 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../app/layout/App";
-import HomePage from "../features/home/HomePage";
 import ActivityForm from "../features/activities/form/ActivityForm";
 import ActivityDashboard from "../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../features/activities/details/ActivityDetails";
@@ -10,10 +9,9 @@ export const routes: RouteObject[] = [
     path: "/",
     element: <App />,
     children: [
-      { path: "", element: <HomePage /> },
       { path: "activities", element: <ActivityDashboard /> },
       { path: "activities/:id", element: <ActivityDetails /> },
-      // "create" key allows us to select empty form via the "createActivity" route if we happen to be editing an activity via the "manage/:id" route
+      // "create" key necessary to select empty form via the "createActivity" route if we happen to be editing <ActivityForm via the "manage/:id" route
       { path: "createActivity", element: <ActivityForm key="create" /> },
       { path: "manage/:id", element: <ActivityForm key="manage" /> },
     ],
