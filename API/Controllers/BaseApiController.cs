b833a4010308c107698efda1bcd115f14953674b
Casey Carroll <caseydean8@gmail.com>
2023-07-11 16:54:13 -0400

    Handle API Error with Result object

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,5 +15,14 @@ namespace API.Controllers
         // We're creating a private variable for mediator and we're going to populate it with the mediator service.
         // If some other controller comes along and decides it also needs mediator, then if it's got it in this property, great. If not, it's going to go and get it again.
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess && result.Value != null)
+                return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
     }
 }
--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using MediatR;
 using Persistence;
@@ -7,7 +8,7 @@ namespace Application.Activities
     public class Details
     {
         // Query fetches, doesn't update
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<Result<Activity>>
         {
             // Set to public so we have access in our handler
             public Guid Id { get; set; }
@@ -15,7 +16,7 @@ namespace Application.Activities
 
         // pass Query as the type of request to be handled and Activity as the type of response from the handler. Then...
         // 1. implement interface for IRequestHandler, see 29
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, Result<Activity>>
         {
             private readonly DataContext _context;
 
@@ -27,14 +28,12 @@ namespace Application.Activities
                 _context = context;
             }
             // 2. Designate Task (implemented from IRequestHandler) as async
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
                 // 6. Return Id (see 13) from request
                 var activity = await _context.Activities.FindAsync(request.Id);
 
-                if (activity == null) throw new Exception("Activity not found");
-
-                return activity;
+                return Result<Activity>.Success(activity);
             }
         }
     }
