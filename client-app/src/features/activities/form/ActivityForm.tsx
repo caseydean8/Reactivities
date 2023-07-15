05f40037cd3f0d34f1ab7d4bf5a1f5e975b75391
Casey Carroll <caseydean8@gmail.com>
2023-07-15 18:55:25 -0400

    Reusable select input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,17 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { Button, FormField, Label, Segment } from "semantic-ui-react";
-import { useStore } from "../../../app/stores/store";
-import { observer } from "mobx-react-lite";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
-import { v4 as uuid } from "uuid";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import MyTextInput from "../../../app/common/form/MyTextInput";
-import MyTextArea from "../../../app/common/form/MyTextArea";
+import { ChangeEvent, useEffect, useState } from 'react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Activity } from '../../../app/models/activity';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { v4 as uuid } from 'uuid';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
+import MySelectInput from '../../../app/common/form/MySelectInput';
+import { categoryOptions } from '../../../app/common/options/categoryOptions';
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
@@ -19,18 +21,18 @@ export default observer(function ActivityForm() {
   const navigate = useNavigate();
 
   const [activity, setActivity] = useState<Activity>({
-    id: "",
-    title: "",
-    category: "",
-    description: "",
-    date: "",
-    city: "",
-    venue: "",
+    id: '',
+    title: '',
+    category: '',
+    description: '',
+    date: '',
+    city: '',
+    venue: '',
   });
 
   const validationSchema = Yup.object({
-    title: Yup.string().required("The activity title is required"),
-    description: Yup.string().required("The activity description is required"),
+    title: Yup.string().required('The activity title is required'),
+    description: Yup.string().required('The activity description is required'),
     category: Yup.string().required(),
     date: Yup.string().required(),
     venue: Yup.string().required(),
@@ -77,7 +79,7 @@ export default observer(function ActivityForm() {
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
             <MyTextInput name='title' placeholder='Title' />
             <MyTextArea rows={3} placeholder='Description' name='description' />
-            <MyTextInput placeholder='Category' name='category' />
+            <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
             <MyTextInput placeholder='Date' name='date' />
             <MyTextInput placeholder='City' name='city' />
             <MyTextInput placeholder='Venue' name='venue' />
