a96f18972043de3275f33f200f33e4c5119f6689
Casey Carroll <caseydean8@gmail.com>
2023-07-19 21:23:34 -0400

    hook up from submission

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,12 @@
-import { ChangeEvent, useEffect, useState } from 'react';
-import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Activity } from '../../../app/models/activity';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
@@ -45,38 +45,28 @@ export default observer(function ActivityForm() {
     if (id) loadActivity(id).then(activity => setActivity(activity!));
   }, [id, loadActivity]);
 
-  //   function handleSubmit() {
-  //     // Generate uuid on the client side for immediate redirect
-  //     if (!activity.id) {
-  //       activity.id = uuid();
-  //       createActivity(activity).then(() =>
-  //         navigate(`/activities/${activity.id}`)
-  //       );
-  //     } else {
-  //       updateActivity(activity).then(() =>
-  //         navigate(`/activities/${activity.id}`)
-  //       );
-  //     }
-  //   }
-
-  //   function handleInputChange(
-  //     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  //   ) {
-  //     const { name, value } = event.target;
-  //     setActivity({ ...activity, [name]: value });
-  //   }
+  function handleFormSubmit(activity: Activity) {
+    // Generate uuid on the client side for immediate redirect
+    if (!activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+    } else {
+      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+    }
+  }
 
   if (loadingInitial) return <LoadingComponent content='totally loading...' />;
 
   return (
     <Segment clearing>
+      <Header content='Activity Details' color='teal' />
       <Formik
         validationSchema={validationSchema}
         enableReinitialize
         initialValues={activity}
-        onSubmit={values => console.log(values)}
+        onSubmit={values => handleFormSubmit(values)}
       >
-        {({ values: activity, handleChange, handleSubmit }) => (
+        {({ handleSubmit, isValid, isSubmitting, dirty }) => (
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
             <MyTextInput name='title' placeholder='Title' />
             <MyTextArea rows={3} placeholder='Description' name='description' />
@@ -88,9 +78,17 @@ export default observer(function ActivityForm() {
               timeCaption='time'
               dateFormat='MMMM d, yyyy h:mm aa'
             />
+            <Header content='Location Details' sub color='teal' />
             <MyTextInput placeholder='City' name='city' />
             <MyTextInput placeholder='Venue' name='venue' />
-            <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+            <Button
+              disabled={isSubmitting || !dirty || !isValid}
+              loading={loading}
+              floated='right'
+              positive
+              type='submit'
+              content='Submit'
+            />
             <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
           </Form>
         )}
