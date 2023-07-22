3b9f11f23bb2b7031d7782b1f781b08e58566767
Casey Carroll <caseydean8@gmail.com>
2023-07-22 16:33:06 -0400

    Added IdentityDbContext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,10 +1,13 @@
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
     // A DbContext instance represents a session with the database and can be used to query and save instances of your entities. DbContext is a combination of the Unit Of Work and Repository patterns.
-    public class DataContext : DbContext
+    // public class DataContext : DbContext
+    // Changed after adding AppUser to our domain, now we are building a multi user application
+    public class DataContext : IdentityDbContext<AppUser>
 
     {
         public DataContext(DbContextOptions options) : base(options)
