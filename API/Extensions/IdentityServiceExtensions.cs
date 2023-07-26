228265428268caa378aabd77764a65c8b9ab0a8f
Casey Carroll <caseydean8@gmail.com>
2023-07-26 18:47:02 -0400

    Register new users

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -13,6 +13,7 @@ namespace API.Extensions
             services.AddIdentityCore<AppUser>(opt =>
             {
                 opt.Password.RequireNonAlphanumeric = false;
+                opt.User.RequireUniqueEmail = true;
             })
             .AddEntityFrameworkStores<DataContext>();
 
