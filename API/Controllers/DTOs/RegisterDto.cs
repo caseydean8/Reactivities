3998a384d3199bcb876df42b533ce78805698dd5
Casey Carroll <caseydean8@gmail.com>
2023-07-27 21:05:22 -0400

    Validate user registrations

--- API/Controllers/DTOs/RegisterDto.cs
+++ API/Controllers/DTOs/RegisterDto.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Controllers.DTOs
 {
     public class RegisterDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        // Regex specifies password must contain a number, uppercase and lowercase letter, and be between 4 and 8 characters
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must meet criteria")]
+        public string Password { get; set; }
+        [Required]
         public string Username { get; set; }
     }
 }
