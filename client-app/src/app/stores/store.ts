97b7666832d462e2a0e7571da2517b862248ab29
Casey Carroll <caseydean8@gmail.com>
2023-07-29 19:05:44 -0400

    Created user store

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,16 +1,19 @@
-import { createContext, useContext } from "react";
-import ActivityStore from "./activityStore";
-import CommonStore from "./commonStore";
+import { createContext, useContext } from 'react';
+import ActivityStore from './activityStore';
+import CommonStore from './commonStore';
+import UserStore from './userStore';
 
 interface Store {
   activityStore: ActivityStore;
   commonStore: CommonStore;
+  userStore: UserStore;
 }
 
 export const store: Store = {
   // As we create new stores we're going to be adding new instances of these stores into this object
   activityStore: new ActivityStore(),
   commonStore: new CommonStore(),
+  userStore: new UserStore(),
 };
 
 // <StoreContext.Provider value={store}> in index.tsx
