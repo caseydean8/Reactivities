0151b20112290319ef7df4b45b5a4ce0685e0ec0
Casey Carroll <caseydean8@gmail.com>
2023-08-26 15:06:09 -0400

    Added register form

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import LoginForm from '../users/LoginForm';
+import RegisterForm from '../users/RegisterForm';
 
 export default observer(function HomePage() {
   const { userStore, modalStore } = useStore();
@@ -32,7 +33,7 @@ export default observer(function HomePage() {
               Login
             </Button>
             <Button
-              onClick={() => modalStore.openModal(<h1>Register</h1>)}
+              onClick={() => modalStore.openModal(<RegisterForm />)}
               as={Link}
               to='/login'
               size='huge'
