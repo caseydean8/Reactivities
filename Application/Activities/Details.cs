94db269e3d73ac68b439d940505d7bd6ba2bb889
Casey Carroll <caseydean8@gmail.com>
2023-09-26 20:20:26 -0400

    Replace eager loading with projection

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,6 +1,9 @@
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -8,7 +11,7 @@ namespace Application.Activities
     public class Details
     {
         // Query fetches, doesn't update
-        public class Query : IRequest<Result<Activity>>
+        public class Query : IRequest<Result<ActivityDto>>
         {
             // Set to public so we have access in our handler
             public Guid Id { get; set; }
@@ -16,24 +19,28 @@ namespace Application.Activities
 
         // pass Query as the type of request to be handled and Activity as the type of response from the handler. Then...
         // 1. implement interface for IRequestHandler, see 29
-        public class Handler : IRequestHandler<Query, Result<Activity>>
+        public class Handler : IRequestHandler<Query, Result<ActivityDto>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
             // 3. Generate constructor from Handler
             // 4. Inject DataContext from Persistence
             // 5. Initialize field from parameter context
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
             // 2. Designate Task (implemented from IRequestHandler) as async
-            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                 // 6. Return Id (see 13) from request
-                var activity = await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
 
-                return Result<Activity>.Success(activity);
+                return Result<ActivityDto>.Success(activity);
             }
         }
     }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,6 @@
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -23,12 +24,10 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                .Include(a => a.Attendees)
-                .ThenInclude(u => u.AppUser)
-                .ToListAsync(cancellationToken);
-                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
 
-                return Result<List<ActivityDto>>.Success(activitiesToReturn);
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
     }
